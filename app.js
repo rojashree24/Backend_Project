f8479a6caed9df31b912ca48cd62693f100e13ab
Rojashree <82460706+rojashree24@users.noreply.github.com>
2022-08-24 00:44:08 +0530

    done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// if(process.env.NODE_ENV!=='production'){
+//     require('dotenv').load()
+// }
 const express=require('express');
 const expressLayout=require('express-ejs-layouts')
 const mongoose=require('mongoose');
-const dotenv=require('dotenv')
+const dotenv=require('dotenv').config();
 
 const indexRouter=require('./routes/index.js')//importing
 const app=express();
@@ -11,8 +14,10 @@ app.set('layout','layouts/layout');
 app.use(expressLayout);
 app.use(express.static('public'));
 
-
-mongoose.connect("mongodb+srv://Rojashree:Rojashree@cluster0.jasmsbk.mongodb.net/?retryWrites=true&w=majority");
+const database=process.env.DB_URL;
+console.log(database);
+mongoose.connect(database);
+// console.log(database);
 const db=mongoose.connection;
 db.on('error',error => console.error(error));
 db.once('open',()=> console.log('connected to DB'));
